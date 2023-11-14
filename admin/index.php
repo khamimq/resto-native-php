d40f61685ba4da1c999617f5f9839ab5e71ad567
khamimq <khamim.q@gmail.com>
2023-11-14 08:13:21 +0700

    resto native php

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../plugins/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styleadmin.css">
+</head>
+<body>
+    <div class="sidebar">
+        <ul>
+            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li><a href="#"><i class="fa fa-users"></i> Petugas</a></li>
+            <li><a href="#"><i class="fa fa-cube"></i> Kategori</a></li>
+            <li><a href="#"><i class="fa fa-cog"></i> Menu</a></li>
+            <li><a href="#"><i class="fa fa-cog"></i> Transaksi</a></li>
+
+        </ul>
+    </div>
+    <div class="content">
+        <div class="topbar">
+            <h1>Admin Dashboard</h1>
+            <a href="#" class="logout">Logout</a>
+        </div>
+        <p>Welcome to the admin dashboard. You can manage your site here.</p>
+    </div>
+</body>
+</html>
